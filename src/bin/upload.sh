425e23c00a6f6898ad88b23d98673243fb7fa673
fnakamura <fujiki.nakamura@gmail.com>
2020-01-13 22:58:55 +0900

    add bin/upload.sh

---
+++ src/bin/upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+logdir="../logs"
+expid=""
+kaggle datasets init -p "${logdir}/${expid}/"
+sed -i -e "s/INSERT_SLUG_HERE/Bengali.AI_${expid}/g" "${logdir}/${expid}/dataset-metadata.json"
+sed -i -e "s/INSERT_TITLE_HERE/Bengali.AI_${expid}/g" "${logdir}/${expid}/dataset-metadata.json"
+kaggle datasets create -p "${logdir}/${expid}/"
